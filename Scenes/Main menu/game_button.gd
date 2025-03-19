de56a3148eb21c0143d30ad8bd13facbdb60b71c
Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>
2025-03-19 22:45:29 +0200

    Link Tetris to main menu and add shape patterns (#11)

--- Scenes/Main menu/game_button.gd
+++ Scenes/Main menu/game_button.gd
@@ -8,9 +8,9 @@ func _ready() -> void:
 	if game_scene.is_empty():
 		printerr("No scene given to button \"%s\"" % self.text)
 		return
-	scene = load(game_scene)
 		
 func _on_pressed() -> void:
+	scene = load(game_scene)
 	print("Pressed \"%s\" button!" % self.text)
 	if scene != null:
 		var error: Error = get_tree().change_scene_to_packed(scene)
