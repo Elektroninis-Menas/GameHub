40d8d29de41b629e2ed2da081f43bdc49204f9c7
ugnauk <ugnauk@ktu.lt>
2025-05-06 20:53:26 +0300

    Janitor
    Clean clean broom broom

--- Tests/Example/test_example.gd
+++
@@ -1,9 +0,0 @@
-extends GutTest
-
-func test_1():
-	# this test will pass because 1 does equal 1
-	assert_eq(1, 1)
-
-func test_2():
-	# this test will pass because 'hello' does not equal 'goodbye'
-	assert_ne('hello', 'goodbye')
