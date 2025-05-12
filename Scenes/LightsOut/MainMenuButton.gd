c0e87ab6da0aba6c8405a7573cd4c8f9650a0fa1
RamVen1 <ramven1@ktu.lt>
2025-05-12 15:44:05 +0300

    Added Save File reset
    Added a button to reset save files

--- Scenes/LightsOut/MainMenuButton.gd
+++ Scenes/LightsOut/MainMenuButton.gd
@@ -8,8 +8,15 @@ var scene: PackedScene
 @onready var easy: Button = $"../ColorRect/Easy"
 @onready var medium: Button = $"../ColorRect/Medium"
 @onready var difficult: Button = $"../ColorRect/Difficult"
+@onready var reset_menu: PopupPanel = $"../ResetMenu"
+@onready var reset_button_confirm: Button = $"../ResetButtonConfirm"
+@onready var reset_text: RichTextLabel = $"../ResetMenu/ResetText"
+@onready var rules_button: Button = $"../RulesButton"
+@onready var main_menu_button: Button = $"."
+
 
 var can_toggle: bool = true
+var rule_toggle: bool =true
 
 func _ready() -> void:
 	if game_scene.is_empty():
@@ -37,3 +44,25 @@ func _on_rules_menu_visibility_changed() -> void:
 	easy.visible = !easy.visible
 	medium.visible = !medium.visible
 	difficult.visible = !difficult.visible
+
+
+func _on_reset_scores_button_button_down() -> void:
+	if rule_toggle:
+		reset_menu.visible = !reset_menu.visible
+		rule_toggle = false
+		get_tree().create_timer(0.5).timeout.connect(func(): rule_toggle = true)
+
+
+func _on_reset_menu_visibility_changed() -> void:
+	reset_text.visible = !reset_text.visible
+	reset_button_confirm.visible = !reset_button_confirm.visible
+	label.visible = !label.visible
+	easy.visible = !easy.visible
+	medium.visible = !medium.visible
+	difficult.visible = !difficult.visible
+	rules_button.visible = !rules_button.visible
+	main_menu_button.visible = !main_menu_button.visible
+
+func _on_reset_button_confirm_button_down() -> void:
+	DirAccess.remove_absolute("user://high_scores.json")
+	print("Save data Deleted")
