5524be10fd69c29702e178af11a47295d9b616e8
Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>
2025-05-21 22:08:55 +0300

    Guess: Update tests

--- test/Guess/test_Guess_game.gd
+++ test/Guess/test_Guess_game.gd
@@ -1,6 +1,7 @@
 extends GutTest
 
-const WORDS_PATH = "res://Scenes/GuessingGame/a_words.txt"
+const A_WORDS_PATH = "res://Scenes/GuessingGame/words/a_words.txt"
+const ALL_WORDS_PATH = "res://Scenes/GuessingGame/words"
 var game: Control
 
 func before_each():
@@ -11,17 +12,16 @@ func before_each():
 	game.submit_button = mock_button
 
 func test_load_word_list_returns_array():
-	var result = game.load_word_list(WORDS_PATH)
+	var result = game.load_word_list(A_WORDS_PATH)
 	assert_typeof(result, TYPE_ARRAY, "Should return an array")
 
 
 func test_load_word_list_contains_strings():
-	var result = game.load_word_list(WORDS_PATH)
-	if result.size() > 0:
-		assert_typeof(result[0], TYPE_STRING, "Array elements should be strings")
+	var result: Array[String] = game.load_word_list(A_WORDS_PATH)
+	assert_typeof(result[0], TYPE_STRING, "Array elements should be strings")
 
 func test_load_word_list_filters_correctly():
-	var result = game.load_word_list(WORDS_PATH)
+	var result = game.load_word_list(A_WORDS_PATH)
 	assert_false(result.is_empty(), "Word list should not be empty")
 	
 	for word in result:
@@ -31,16 +31,16 @@ func test_load_word_list_filters_correctly():
 		assert_true(word.is_valid_filename(), "Words should contain only alphabetic characters")
 
 func test_word_list_path_is_correct():
-	assert_eq(game.WORDS_PATH, "res://Scenes/GuessingGame/a_words.txt", "Word list path should be correct");
+	assert_eq(game.WORDS_PATH, "res://Scenes/GuessingGame/words", "Word list path should be correct");
 	
 func test_file_access_success():
-	var file = FileAccess.open(game.WORDS_PATH, FileAccess.READ)
+	var file = FileAccess.open(game.WORDS_PATH + "/a_words.txt", FileAccess.READ)
 	assert_not_null(file,"Should be able to open word list file")
 	if file:
 		file.close()
 		
 func test_word_list_no_duplicates():
-	var result: Array[String] = game.load_word_list(WORDS_PATH)
+	var result: Array[String] = game.load_word_list(A_WORDS_PATH)
 	var unique_words: Array[String] = []
 	for word in result:
 		assert_false(unique_words.has(word),"Word list should not contain duplicates")
@@ -48,12 +48,12 @@ func test_word_list_no_duplicates():
 	
 	
 func test_repeated_loading_consistency():
-	var first_load = game.load_word_list(WORDS_PATH)
-	var second_load = game.load_word_list(WORDS_PATH)
+	var first_load = game.load_word_list(A_WORDS_PATH)
+	var second_load = game.load_word_list(A_WORDS_PATH)
 	assert_eq(first_load, second_load, "Repeated loads should return same words")
 	
 func test_word_list_contains_only_letters():
-	var result = game.load_word_list(WORDS_PATH)
+	var result = game.load_word_list(A_WORDS_PATH)
 	var regex = RegEx.new()
 	regex.compile("^[a-zA-Z]+$")  
 	
