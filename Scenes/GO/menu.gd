dc25bb7db298b126a2cbc795990c82a9852c7169
ugnauk <ugnauk@ktu.lt>
2025-05-22 00:34:15 +0300

    Go minor changes (#55)
    * Go minor changes
    
    new main screen
    functional buttons
    
    * Stuff
    
    * Still fixing
    
    * Done
    
    ---------
    
    Co-authored-by: Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>

--- Scenes/GO/menu.gd
+++ Scenes/GO/menu.gd
@@ -4,11 +4,6 @@ extends Control
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/GO/New_Game_Scene/New_Game_Scene.tscn")
 
-
-func _on_settings_pressed() -> void:
-	pass  # Replace with function body.
-
-
 func _on_rules_pressed() -> void:
 	$"../Window".popup_centered()
 
